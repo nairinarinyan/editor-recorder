442fd9f44256b894a46b92175a8d1ee319d9ad0c
Nairi Narinyan <nairi.narinyan@gmail.com>
2017-09-27 18:30:25 +0400

    try reset

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,28 @@
 import { getElements, toggleClasses, initTouchEventHandlers } from './dom';
 import Recorder from './recorder';
+import DrawProgress from './draw-progress'
 
-const [button, canvas, controlPanel] = getElements('#record-button', '#canvas', '.control-panel.record');
+const [button, canvas, controlPanel] = getElements('#record-button', '#canvas', '#control-panel');
 
 const canvasCtx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth * .9;
 
 const recorder = new Recorder(canvasCtx);
+const drawProgress = new DrawProgress();
 
 controlPanel.addEventListener('click', () => {
     toggleClasses('recording', button, controlPanel);
 
-    !recorder.isRunning ?
-        recorder.start() :
+    if (!recorder.isRunning) {
+        recorder.start();
+        // drawProgress.play();
+    } else {
         recorder.stop();
+        // drawProgress.stop();
+    }
 });
 
 initTouchEventHandlers(canvas, (startRatio, endRatio) => {
-      
+        
 });
