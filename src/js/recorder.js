1a159585fda10a3eac9a7e60252ce7408009bcdc
Bas Horsting <bas.horsting@greetz.com>
2017-09-27 11:17:12 +0200

    Add basic recording settings

--- src/js/recorder.js
+++ src/js/recorder.js
@@ -5,16 +5,43 @@ export default class Recorder {
 
     start() {
         this.audioCtx = new AudioContext();
-
-        navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+        navigator.mediaDevices.getUserMedia(
+            {
+                video: true,
+                audio: {
+                    echoCancellation: true, // disabling audio processing
+                    googAutoGainControl: true,
+                    autoGainControl: true,
+                    googNoiseSuppression: true,
+                    noiseSuppression: true,
+                    channelCount:1,
+                    googHighpassFilter: true,
+                    googTypingNoiseDetection: true,
+                    echoCancellation: true,
+                    sampleSize: 16,
+                    sampleRate: 16000
+                }
+            }
+        )
             .then(stream => {
-                this.audioStream = stream;
-                this.isRunning = true;
+            this.audioStream = stream;
+        this.isRunning = true;
 
-                const input = this.audioCtx.createMediaStreamSource(stream);
-                input.connect(this.audioCtx.destination);
-            })
-            .catch(console.error);
+        const input = this.audioCtx.createMediaStreamSource(stream);
+
+        var compressor = this.audioCtx.createDynamicsCompressor();
+        compressor.threshold.value = -50;
+        compressor.knee.value = 40;
+        compressor.ratio.value = 12;
+        compressor.attack.value = 0;
+        compressor.release.value = 0.25;
+
+        input.connect(compressor);
+        compressor.connect(this.audioCtx.destination);
+
+    })
+    .
+        catch(console.error);
     }
 
     stop() {
