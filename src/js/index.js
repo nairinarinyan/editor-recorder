971cb00c416fe05dc65957fc0c6bfb07aa8a1146
ggasparyan.gayane@gmail.com <ggasparyan.gayane@gmail.com>
2017-09-29 10:53:40 +0400

    btns interaction changes

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import { getElements, toggleClasses, initTouchEventHandlers } from './dom';
 import Recorder from './recorder';
 import DrawProgress from './draw-progress'
 
-const [button, canvas, controlPanel] = getElements('#record-button', '#canvas', '#control-panel');
+const [recordButton, canvas, controlPanel] = getElements('#record-button', '#canvas', '#control-panel');
 
 const canvasCtx = canvas.getContext('2d');
 
@@ -13,8 +13,8 @@ const drawProgress = new DrawProgress();
 
 canvas.width = window.innerWidth * .9;
 
-controlPanel.addEventListener('click', () => {
-    toggleClasses('recording', button, controlPanel);
+recordButton.addEventListener('click', () => {
+    toggleClasses('recording', recordButton, controlPanel);
 
     if (!recorder.isRunning) {
         recorder.start();
