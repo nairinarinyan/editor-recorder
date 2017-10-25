380af31ca4c42e1428aee45e7e52885c83d42012
Nairi Narinyan <nairi.narinyan@gmail.com>
2017-10-25 12:49:35 +0400

    crop anywhere

--- src/js/dom.js
+++ src/js/dom.js
@@ -24,7 +24,6 @@ export function showNotification(message, error) {
 
 export function initTouchEventHandlers(canvas, cb) {
     const parent = canvas.parentElement;
-    let touched = false;
     let initialPosition;
     let delta;
     const overLayElement = document.querySelector('.overlay');
@@ -32,41 +31,34 @@ export function initTouchEventHandlers(canvas, cb) {
 
     let x;
 
-    parent.addEventListener('touchstart', () => {
-        // console.log('touchstart');
-        touched = true;
+    parent.addEventListener('touchstart', evt => {
+        x = evt.changedTouches[0].clientX;
+
+        initialPosition = x;
+        overLayElement.style.transform = `translate(${initialPosition - boundingBox.left}px)`;
+        overLayElement.style.display = 'block';
+        overLayElement.style.width = 0;
     });
 
     parent.addEventListener('touchend', evt => {
         const startRatio =  (initialPosition - boundingBox.left) / boundingBox.width;
-
         const endRatio =  (x - boundingBox.left) / boundingBox.width;
-
         const durationRatio = delta / boundingBox.width;
 
         cb(startRatio, endRatio, durationRatio);
-
-        touched = false;
     });
 
     parent.addEventListener('touchmove', evt => {
         x = evt.changedTouches[0].clientX;
 
-        if (!initialPosition) {
-            initialPosition = x;
-            overLayElement.style.transform = `translate(${initialPosition - boundingBox.left}px)`;
-            overLayElement.style.display = 'block';
-            overLayElement.style.width = 0;
-        } else {
-            if (x > boundingBox.left + boundingBox.width) {
-                x = boundingBox.width + boundingBox.left;
-            }
-
-            delta = x - initialPosition;
-
-            requestAnimationFrame(() => {
-                overLayElement.style.width = delta + 'px';
-            });
+        if (x > boundingBox.left + boundingBox.width) {
+            x = boundingBox.width + boundingBox.left;
         }
+
+        delta = x - initialPosition;
+
+        requestAnimationFrame(() => {
+            overLayElement.style.width = delta + 'px';
+        });
     });
 }
